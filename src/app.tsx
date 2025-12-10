ee6e57a79968ee6bf136016a2a47dec114f8ed0d
Todor Imreorov <blurymind@gmail.com>
2025-12-10 17:38:58 +0000

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,28 @@
+import { RouterProvider } from "@tanstack/react-router";
+import { router } from "./main";
+// import { AuthProvider, useAuth } from "./providers/auth-provider";
+
+function InnerApp() {
+  // const auth = useAuth();
+
+  // if (auth.loading) {
+  //   return (
+  //     <div className="flex h-screen w-full items-center justify-center p-4">
+  //       <span className="loading loading-spinner text-primary"></span>
+  //     </div>
+  //   );
+  // }
+
+  return <RouterProvider router={router}
+                         // context={{ auth }}
+  />;
+}
+
+export function App() {
+  return <InnerApp />;
+  // return (
+  //   <AuthProvider>
+  //     <InnerApp />
+  //   </AuthProvider>
+  // );
+}
