daffe064b4d47e5efb6172aa752a740b4497b8fe
blurymind <blurymind@gmail.com>
2026-01-12 22:45:59 +0000

    fix some iframes mess with resize view events

--- src/shared/utils/initData.ts
+++ src/shared/utils/initData.ts
@@ -1,25 +1,29 @@
 export const newTemplate = `// code your react component below
- ({import: {SplitView, View, PiskelReact}}) => {
-     const ref= useRef(null)
- console.log({PiskelReact})
+// code your react component below
+({
+  import: {
+    SplitView, View, PiskelReact
+  }}) => {
+  const ref = useRef(null)
+  console.log({
+    PiskelReact
+  })
   return (
     <div className="flex flex-1 flex-col h-full w-full bg-gray-100">
       <SplitView>
-      <View>
-  
-         <PiskelReact ref={ref}/>
-         </View>
-      <View>
-  
-        <iframe 
-         src="https://blurymind.github.io/YarnClassic"
-         width="100%"
-         height="100%"
-        />
-         </View>
-    </SplitView>
+        <View>
+          <PiskelReact ref={ref} />
+        </View>
+        <View>
+          <iframe
+            src="https://blurymind.github.io/YarnClassic"
+            width="100%"
+            height="100%"
+            />
+        </View>
+      </SplitView>
     </div>
   )
- }
+}
 `;
 export const initDataEditors = { piskel: newTemplate };
